9c11c1f118e1e0835bc9c11662629f6eb6a30517
Aaditya Sharma <163049815+AadiSharma49@users.noreply.github.com>
2025-01-08 10:46:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,44 +6,20 @@ import Skill from './Components/Skill';
 import Project from './Components/Project';
 import ContactForm from './Components/ContactForm';
 import Footer from './Components/Footer';
-import { useEffect } from 'react';
-
 const App = () => {
-  useEffect(() => {
-    // Disable horizontal scrolling
-    document.body.style.overflowX = 'hidden';
-
-    // Cleanup on unmount
-    return () => {
-      document.body.style.overflowX = '';
-    };
-  }, []); // Empty array ensures this effect runs only once
-
   return (
-   <div className="relative h-full overflow-y-auto antialiased">
+    <div className="relative h-full overflow-y-auto antialiased">
       <div className='fixed inset-0 bg-fixed bg-cover bg-center bg-img'></div>
       <div className='relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto'>
-     <Navbar/>
+      <Navbar/>
       <Hero/>
       <About/>
       <Skill/>
       <Project/>
       <ContactForm/>
       <Footer/>
-        </div>
-     </div>
-  );
-};
-
-export default App;
-
-
-
-
-
-
-
-
-
-
- 
+      </div>
+    </div>
+  )
+}
+export default App
